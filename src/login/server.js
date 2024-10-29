059c09761b6e7955e4323eb882b35110c4a10bcc
Laxmikant Mahindrakar <111036655+Laxmikant143Mahi@users.noreply.github.com>
2024-10-29 10:40:02 +0530

    Update server.js

--- src/login/server.js
+++ src/login/server.js
@@ -16,7 +16,7 @@ const path = require('path');
 const app = express();
 app.use(express.json());
 
-const PORT = process.env.PORT || 10000;
+const PORT = process.env.PORT || 5000;
 app.use(cors());
 
 
